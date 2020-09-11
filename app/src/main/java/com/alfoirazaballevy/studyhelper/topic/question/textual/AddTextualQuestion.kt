ec4ec3a5e7b8110a619460cc533140671dbb19c2
alfoirazabal <alfoirazabal@live.com>
2020-09-11 15:08:49 -0300

    Using translations (NEW), Textual question controls (NEW)

--- app/src/main/java/com/alfoirazaballevy/studyhelper/topic/question/textual/AddTextualQuestion.kt
+++ app/src/main/java/com/alfoirazaballevy/studyhelper/topic/question/textual/AddTextualQuestion.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alfoirazaballevy.studyhelper.R
 import com.alfoirazaballevy.studyhelper.db.DbHelper
@@ -37,12 +38,32 @@ class AddTextualQuestion(
         txtTopic.text = topicName
 
         btnAddQuestion.setOnClickListener {
-            val DbHlp = DbHelper(applicationContext)
-            val questionTitle = etxtQuestion.text.toString()
-            val questionAnswer = etxtAnswer.text.toString()
-            val score = etxtScore.text.toString().toFloat()
-            DbHlp.addTextualQuestion(topicId, questionTitle, questionAnswer, score)
-            finish()
+            if (etxtQuestion.text.toString() == "") {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.add_question_required,
+                    Toast.LENGTH_LONG
+                ).show()
+            } else if (etxtAnswer.text.toString() == "") {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.add_answer_required,
+                    Toast.LENGTH_LONG
+                ).show()
+            } else if (etxtScore.text.toString() == "") {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.add_score_required,
+                    Toast.LENGTH_LONG
+                )
+            } else {
+                val DbHlp = DbHelper(applicationContext)
+                val questionTitle = etxtQuestion.text.toString()
+                val questionAnswer = etxtAnswer.text.toString()
+                val score = etxtScore.text.toString().toFloat()
+                DbHlp.addTextualQuestion(topicId, questionTitle, questionAnswer, score)
+                finish()
+            }
         }
 
     }
